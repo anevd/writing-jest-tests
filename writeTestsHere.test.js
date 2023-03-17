35c4794e4374b71de150fc35cc803480e4a2b627
mkarsums15@mail.ru <mkarsums15@mail.ru>
2023-03-17 16:42:38 +0300

    add different methods

--- writeTestsHere.test.js
+++ writeTestsHere.test.js
@@ -4,25 +4,25 @@ const func3 = require("./3.js");
 
 test("getSum should return sum of elements", () => {
 	expect(func1([1, 2, 3])).toBe(6);
-	expect(func1([0, 0, 0, 0, 0])).toBe(0);
-	expect(func1([13, 10, 7])).toBe(30);
-	expect(func1([2, 2, 2])).toBe(6);
+	expect(func1([0, 0, 0, 0, 0])).toEqual(0);
+	expect(func1([13, 10, 7])).toBeLessThan(31);
+	expect(func1([2, 2, 2])).toBeGreaterThan(5);
 	expect(func1([-3, 1, 2])).toBe(0);
 	expect(func1([-10, -5, -5])).toBe(-20);
 });
 
 test("arithmetic should return the result of an arithmetic operation", () => {
 	expect(func2(1, 2, "add")).toBe(3);
-	expect(func2(1, 2, "subtract")).toBe(-1);
-	expect(func2(1, 2, "multiply")).toBe(2);
-	expect(func2(1, 2, "divide")).toBe(0.5);
+	expect(func2(1, 2, "subtract")).toEqual(-1);
+	expect(func2(1, 2, "multiply")).not.toBe(3);
+	expect(func2(1, 2, "divide")).toBeCloseTo(0.5);
 	expect(func2(1, 2, "")).toBe("No such result");
 });
 
 test("checkNumber should check if the number is even", () => {
-	expect(func3(1)).toBe(false);
-	expect(func3(0)).toBe(true);
-	expect(func3(10)).toBe(true);
-	expect(func3(-5)).toBe(false);
-	expect(func3(-20)).toBe(true);
+	expect(func3(1)).toBeFalsy();
+	expect(func3(0)).toBeTruthy();
+	expect(func3(10)).not.toBeFalsy();
+	expect(func3(-5)).toBeFalsy();
+	expect(func3(-20)).toBeTruthy();
 });
